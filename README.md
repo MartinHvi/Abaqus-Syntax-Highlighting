743169cbd0ec3670695d10a7d5a78a7b8688e61c
MartinHvi <martin.hviidd@gmail.com>
2024-09-07 11:44:27 +0200

    fix linting

--- README.md
+++ README.md
@@ -1,30 +1,38 @@
 Unofficial VS Code extension for abaqus input file support.
-***
+
+---
+
 # Features
+
 - [x] Syntax highlighting
 - [x] Custom hover
 - [x] Commands accessible by the command palette using "CTRL + SHIFT + P" and searching for: Abaqus: Indent lines, Abaqus: Format, Abaqus: Compact, Abaqus: Remove leading spaces, Abaqus: Remove all comment, Abaqus: Remove empty comments, Abaqus: Remove blank lines, Abaqus: Uppercase.
 - [x] If the command "Abaqus: Indent lines" is used then all data lines can be folded using "CTRL + K + 0". To unfold use "CTRL + K + J".
-***
+
+---
 
 # Abaqus Syntax Highlighting VSCode
+
 ![DEMO1](https://github.com/MartinHvi/abaqus-syntax-highlighting/blob/main/images/DEMO.gif?raw=true)
 
 The Abaqus Syntax Highlighting extension will add syntax highlighting and make your Abaqus files easier to read. The extension also support custom hover information.
 
 Supported file extensions:
-* .inp
-* .inc
-* .incl
 
-***
+- .inp
+- .inc
+- .incl
+
+---
 
 # How to add custom hover information
+
 Create a .json file with the keys in lowercase and values as a markdown string. An example of a json file with custom hover information can be seem below for the words "default" and "example".
+
 ```json
 {
-	"default": "# Default hover info \n\n This is a **default** hover 🧐",
-	"example": "# Example hover info \n\n This is an **example** 🧐"
+  "default": "# Default hover info \n\n This is a **default** hover 🧐",
+  "example": "# Example hover info \n\n This is an **example** 🧐"
 }
 ```
 
@@ -36,8 +44,9 @@ Create a .json file with the keys in lowercase and values as a markdown string. 
 ![DEMO3](https://github.com/MartinHvi/abaqus-syntax-highlighting/blob/main/images/demoHover.gif?raw=true)
 
 **Alternative 2.** CTRL+Shift+P and select "Preferences: Open User Settings (JSON)". Add the line below with the path to your json file then restart VSCode.
+
 ```json
 {
-	"abaqusSyntaxHighlighting.customHover.pathJSON": "C:\\Users\\Username\\Desktop\\customhover.json"
+  "abaqusSyntaxHighlighting.customHover.pathJSON": "C:\\Users\\Username\\Desktop\\customhover.json"
 }
 ```
--- src/Commands.ts
+++ src/Commands.ts
@@ -1,200 +1,223 @@
 import * as vscode from 'vscode';
 
 export function indentLines() {
-	// Get the current text editor
-	const editor = vscode.window.activeTextEditor;
-	if (!editor) {
-		return; // No open text editor
-	}
-	const document = editor.document;
-	const lineCount = document.lineCount;
-	editor.edit(editBuilder => {
-		for (let i = 0; i < lineCount; i++) {
-			const line = document.lineAt(i);
-			const lineText = line.text;
-			if (!lineText.startsWith('*') && !lineText.startsWith(' ')) {
-				editBuilder.insert(line.range.start, '  ');
-			}
-		}
-	});
+  // Get the current text editor
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    return; // No open text editor
+  }
+  const document = editor.document;
+  const lineCount = document.lineCount;
+  editor.edit((editBuilder) => {
+    for (let i = 0; i < lineCount; i++) {
+      const line = document.lineAt(i);
+      const lineText = line.text;
+      if (!lineText.startsWith('*') && !lineText.startsWith(' ')) {
+        editBuilder.insert(line.range.start, '  ');
+      }
+    }
+  });
 }
 
-
 export function removeLeadingSpaces() {
-	// Get the current text editor
-	let editor = vscode.window.activeTextEditor;
-	if (!editor) {
-		return;
-	}
-	// Get the range of the entire document
-	let documentRange = new vscode.Range(0, 0, editor.document.lineCount - 1, editor.document.lineAt(editor.document.lineCount - 1).text.length);
-	// Get the text of the document
-	let documentText = editor.document.getText(documentRange);
-	// Split the document into lines
-	let lines = documentText.split('\n');
-	// Initialize an array for the modified lines
-	let modifiedLines: string[] = [];
-	// Iterate over the lines
-	for (let i = 0; i < lines.length; i++) {
-		if (lines[i][0] !== '*') {
-			// If the line does not start with an asterisk, remove all leading spaces
-			modifiedLines.push(lines[i].replace(/^\s+/, ''));
-		} else {
-			// If the line starts with an asterisk, leave it unchanged
-			modifiedLines.push(lines[i]);
-		}
-	}
-	// Join the modified lines into a single string
-	let modifiedText = modifiedLines.join('\n');
-	// Replace the document text with the modified text
-	editor.edit(editBuilder => {
-		editBuilder.replace(documentRange, modifiedText);
-	});
+  // Get the current text editor
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    return;
+  }
+  // Get the range of the entire document
+  const documentRange = new vscode.Range(
+    0,
+    0,
+    editor.document.lineCount - 1,
+    editor.document.lineAt(editor.document.lineCount - 1).text.length,
+  );
+  // Get the text of the document
+  const documentText = editor.document.getText(documentRange);
+  // Split the document into lines
+  const lines = documentText.split('\n');
+  // Initialize an array for the modified lines
+  const modifiedLines: string[] = [];
+  // Iterate over the lines
+  for (let i = 0; i < lines.length; i++) {
+    if (lines[i][0] !== '*') {
+      // If the line does not start with an asterisk, remove all leading spaces
+      modifiedLines.push(lines[i].replace(/^\s+/, ''));
+    } else {
+      // If the line starts with an asterisk, leave it unchanged
+      modifiedLines.push(lines[i]);
+    }
+  }
+  // Join the modified lines into a single string
+  const modifiedText = modifiedLines.join('\n');
+  // Replace the document text with the modified text
+  editor.edit((editBuilder) => {
+    editBuilder.replace(documentRange, modifiedText);
+  });
 }
 
 export function format() {
-	// Get the current text editor
-	let editor = vscode.window.activeTextEditor;
+  // Get the current text editor
+  const editor = vscode.window.activeTextEditor;
 
-	if (!editor) {
-		return;
-	}
-	// Get the range of the entire document
-	let documentRange = new vscode.Range(0, 0, editor.document.lineCount - 1, editor.document.lineAt(editor.document.lineCount - 1).text.length);
-	// Get the text of the document
-	let documentText = editor.document.getText(documentRange);
-	// Split the document into lines
-	let lines = documentText.split('\n');
-	// Initialize an array for the modified lines
-	let modifiedLines: string[] = [];
-	// Iterate over the lines
-	for (let i = 0; i < lines.length; i++) {
-		// Remove spaces after commas and add a single space after all commas
-		modifiedLines.push(lines[i].replace(/,\s+/g, ',').replace(/,/g, ', '));
-	}
-	// Join the modified lines into a single string
-	let modifiedText = modifiedLines.join('\n');
-	// Replace the document text with the modified text
-	editor.edit(editBuilder => {
-		editBuilder.replace(documentRange, modifiedText);
-	});
+  if (!editor) {
+    return;
+  }
+  // Get the range of the entire document
+  const documentRange = new vscode.Range(
+    0,
+    0,
+    editor.document.lineCount - 1,
+    editor.document.lineAt(editor.document.lineCount - 1).text.length,
+  );
+  // Get the text of the document
+  const documentText = editor.document.getText(documentRange);
+  // Split the document into lines
+  const lines = documentText.split('\n');
+  // Initialize an array for the modified lines
+  const modifiedLines: string[] = [];
+  // Iterate over the lines
+  for (let i = 0; i < lines.length; i++) {
+    // Remove spaces after commas and add a single space after all commas
+    modifiedLines.push(lines[i].replace(/,\s+/g, ',').replace(/,/g, ', '));
+  }
+  // Join the modified lines into a single string
+  const modifiedText = modifiedLines.join('\n');
+  // Replace the document text with the modified text
+  editor.edit((editBuilder) => {
+    editBuilder.replace(documentRange, modifiedText);
+  });
 }
 
 export function compact() {
-	// Get the current text editor
-	let editor = vscode.window.activeTextEditor;
+  // Get the current text editor
+  const editor = vscode.window.activeTextEditor;
 
-	if (!editor) {
-		return;
-	}
-	// Get the range of the entire document
-	let documentRange = new vscode.Range(0, 0, editor.document.lineCount - 1, editor.document.lineAt(editor.document.lineCount - 1).text.length);
-	// Get the text of the document
-	let documentText = editor.document.getText(documentRange);
-	// Split the document into lines
-	let lines = documentText.split('\n');
-	// Initialize an array for the modified lines
-	let modifiedLines: string[] = [];
-	// Iterate over the lines
-	for (let i = 0; i < lines.length; i++) {
-		// Remove spaces after commas and add a single space after all commas
-		modifiedLines.push(lines[i].replace(/,\s+/g, ',').replace(/^\s+/, ''));
-	}
-	// Join the modified lines into a single string
-	let modifiedText = modifiedLines.join('\n');
-	// Replace the document text with the modified text
-	editor.edit(editBuilder => {
-		editBuilder.replace(documentRange, modifiedText);
-	});
+  if (!editor) {
+    return;
+  }
+  // Get the range of the entire document
+  const documentRange = new vscode.Range(
+    0,
+    0,
+    editor.document.lineCount - 1,
+    editor.document.lineAt(editor.document.lineCount - 1).text.length,
+  );
+  // Get the text of the document
+  const documentText = editor.document.getText(documentRange);
+  // Split the document into lines
+  const lines = documentText.split('\n');
+  // Initialize an array for the modified lines
+  const modifiedLines: string[] = [];
+  // Iterate over the lines
+  for (let i = 0; i < lines.length; i++) {
+    // Remove spaces after commas and add a single space after all commas
+    modifiedLines.push(lines[i].replace(/,\s+/g, ',').replace(/^\s+/, ''));
+  }
+  // Join the modified lines into a single string
+  const modifiedText = modifiedLines.join('\n');
+  // Replace the document text with the modified text
+  editor.edit((editBuilder) => {
+    editBuilder.replace(documentRange, modifiedText);
+  });
 }
 export function removeAllComments() {
-	const editor = vscode.window.activeTextEditor;
-	if (!editor) {
-		return;
-	}
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    return;
+  }
 
-	const document = editor.document;
+  const document = editor.document;
 
-	// Get the range of lines that should be removed
-	const startLine = 0;
-	const endLine = document.lineCount - 1;
-	const lineRange = new vscode.Range(startLine, 0, endLine, 0);
+  // Get the range of lines that should be removed
+  const startLine = 0;
+  const endLine = document.lineCount - 1;
+  const lineRange = new vscode.Range(startLine, 0, endLine, 0);
 
-	// Get the text of the lines that should be removed
-	const text = document.getText(lineRange);
+  // Get the text of the lines that should be removed
+  const text = document.getText(lineRange);
 
-	// Split the text into lines and filter out the lines starting with **
-	const lines = text.split('\n').filter((line) => !line.startsWith('**'));
+  // Split the text into lines and filter out the lines starting with **
+  const lines = text.split('\n').filter((line) => !line.startsWith('**'));
 
-	// Replace the original text with the filtered text
-	editor.edit((editBuilder) => {
-		editBuilder.replace(lineRange, lines.join('\n'));
-	});
+  // Replace the original text with the filtered text
+  editor.edit((editBuilder) => {
+    editBuilder.replace(lineRange, lines.join('\n'));
+  });
 }
 
 export function removeEmptyComments() {
-	const editor = vscode.window.activeTextEditor;
-	if (!editor) {
-		return;
-	}
-	let document = editor.document;
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    return;
+  }
+  const document = editor.document;
 
-	let start = new vscode.Position(0, 0);
-	let end = new vscode.Position(document.lineCount - 1, document.lineAt(document.lineCount - 1).text.length);
-	let range = new vscode.Range(start, end);
+  const start = new vscode.Position(0, 0);
+  const end = new vscode.Position(
+    document.lineCount - 1,
+    document.lineAt(document.lineCount - 1).text.length,
+  );
+  const range = new vscode.Range(start, end);
 
-	let text = document.getText(range);
-	let lines = text.split('\n');
+  const text = document.getText(range);
+  const lines = text.split('\n');
 
-	let newLines = lines.filter(line => {
-		return !(line.trim() === '**' || line.trim() === '');
-	});
+  const newLines = lines.filter((line) => {
+    return !(line.trim() === '**' || line.trim() === '');
+  });
 
-	let newText = newLines.join('\n');
-	editor.edit(editBuilder => {
-		editBuilder.replace(range, newText);
-	});
+  const newText = newLines.join('\n');
+  editor.edit((editBuilder) => {
+    editBuilder.replace(range, newText);
+  });
 }
 
 export function removeBlankLines() {
-	const editor = vscode.window.activeTextEditor;
-	if (!editor) {
-		return;
-	}
-	let document = editor.document;
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    return;
+  }
+  const document = editor.document;
 
-	let start = new vscode.Position(0, 0);
-	let end = new vscode.Position(document.lineCount - 1, document.lineAt(document.lineCount - 1).text.length);
-	let range = new vscode.Range(start, end);
+  const start = new vscode.Position(0, 0);
+  const end = new vscode.Position(
+    document.lineCount - 1,
+    document.lineAt(document.lineCount - 1).text.length,
+  );
+  const range = new vscode.Range(start, end);
 
-	let text = document.getText(range);
-	let lines = text.split('\n');
+  const text = document.getText(range);
+  const lines = text.split('\n');
 
-	let newLines = lines.filter(line => {
-		return !(line.trim() === '');
-	});
+  const newLines = lines.filter((line) => {
+    return !(line.trim() === '');
+  });
 
-	let newText = newLines.join('\n');
-	editor.edit(editBuilder => {
-		editBuilder.replace(range, newText);
-	});
+  const newText = newLines.join('\n');
+  editor.edit((editBuilder) => {
+    editBuilder.replace(range, newText);
+  });
 }
 
 export function toUppercase() {
-	const editor = vscode.window.activeTextEditor;
-	if (!editor) {
-		return;
-	}
-	let document = editor.document;
+  const editor = vscode.window.activeTextEditor;
+  if (!editor) {
+    return;
+  }
+  const document = editor.document;
 
-	let start = new vscode.Position(0, 0);
-	let end = new vscode.Position(document.lineCount - 1, document.lineAt(document.lineCount - 1).text.length);
-	let range = new vscode.Range(start, end);
+  const start = new vscode.Position(0, 0);
+  const end = new vscode.Position(
+    document.lineCount - 1,
+    document.lineAt(document.lineCount - 1).text.length,
+  );
+  const range = new vscode.Range(start, end);
 
-	let text = document.getText(range);
-	let newText = text.toUpperCase();
+  const text = document.getText(range);
+  const newText = text.toUpperCase();
 
-	editor.edit(editBuilder => {
-		editBuilder.replace(range, newText);
-	});
+  editor.edit((editBuilder) => {
+    editBuilder.replace(range, newText);
+  });
 }
